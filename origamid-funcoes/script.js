a21be60958da3b1531b10c78e40e193d34044f59
Jonatan Nascimento <quadrado4@gmail.com>
2024-04-06 23:02:52 -0300

    Aula e exercicio funções

---
+++ origamid-funcoes/script.js
@@ -0,0 +1,38 @@
+function logCurso() {
+  const nome = "JavaScript";
+  console.log(nome);
+}
+logCurso();
+
+function logCurso2(nome) {
+  console.log(nome);
+}
+logCurso2("JavaScript");
+logCurso2("HTML");
+
+console.log(" ");
+// Exercicio
+
+// function livro(nome, ano, autor) {
+//   const nomeMaior = nome.toUpperCase();
+//   const totalAnos = 2050 - ano;
+//   const frase = nome + " por " + autor;
+//   const objeto = {
+//     nome: nomeMaior,
+//     totalAnos,
+//     frase,
+//   };
+//   return objeto;
+// }
+
+function livro(nome, ano, autor) {
+  return {
+    nome: nome.toUpperCase(),
+    totalAnos: 2050 - ano,
+    frase: nome + " por " + autor,
+  };
+}
+
+const livroRetorno = livro("O Senhor dos Anéis", 1954, "J. R. R. Tolkien");
+
+console.log(livroRetorno);
